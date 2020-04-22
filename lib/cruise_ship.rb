a4e3f7af8de7eb7dbb49dd8847999d3c3a658048
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-22 19:38:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -15,11 +15,12 @@ suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-     end
+  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner += name
+    end
   end
-  
- return winner
+    return winner
+end
