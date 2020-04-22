33637467d9aeedc6a4b6f2e6fb941c2e95341a9a
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-22 19:37:39 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -18,8 +18,7 @@ winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
-    return winner
-  end
+     end
   end
   
-  winner
+ return winner
